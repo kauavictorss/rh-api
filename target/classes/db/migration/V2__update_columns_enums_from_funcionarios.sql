ffa9c26d074feb994fc58a4fd6cd58e04e6df247
Kauã Victor <164793271+kauavictorss@users.noreply.github.com>
2025-05-27 22:27:30 -0300

    Adicionado migration de update

---
+++ target/classes/db/migration/V2__update_columns_enums_from_funcionarios.sql
@@ -0,0 +1,15 @@
+update funcionarios
+set tipo_conta = 'POUPANCA'
+where tipo_conta = 'poupança';
+
+update funcionarios
+set especialidade = 'PROGRAMACAO'
+where especialidade = 'PROGRAMADOR';
+
+update funcionarios
+set tipo_conta = 'CORRENTE'
+where tipo_conta = 'corrente';
+
+update funcionarios
+set especialidade = 'MEDICINA'
+where especialidade = 'MEDICO';
